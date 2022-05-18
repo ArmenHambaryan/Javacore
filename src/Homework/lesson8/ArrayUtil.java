80145ac6398f12c31a0277419d3a79fbbaaccb11
lenovo <hambaryan.93@bk.ru>
2022-05-19 00:33:17 +0400

    lesson 8,ArrayUtil

--- src/Homework/lesson8/ArrayUtil.java
+++ src/Homework/lesson8/ArrayUtil.java
@@ -4,65 +4,71 @@ public class ArrayUtil {
     public static void main(String[] args) {
         //տպել մասիվի բոլոր էլեմենտները;
 
-        //int[]array={126,461,55,67,98,101,12,45,502};
-        //        for (int i = 0; i < array.length; i++) {
-        //            System.out.print(array[i]+" ");
+        int[] array = {126, 461, 55, 67, 98, 101, 12, 45, 502};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
 
-        int[] array = {7, 5, 4, 6, 8, -45, 879, 997, 32, 4, -56};
-        //    int a=array[0];
-        //    for (int i = 0; i < array.length; i++) {
-        //         if (array[i]< a){
-        //          a=array[i];
-        //        }
-        //  }
-        // System.out.println(a);
-        //  int a=array[0];
-        //for (int i = 0; i < array.length; i++) {
-        //   if (array[i]>a){
-        //       a=array[i];
-        //    }
+        System.out.println();
 
-        // }
-        //System.out.println(a);
-        //int a =0;
-        // for (int i = 0; i < array.length; i++) {
-        //    a=a+array[i];
-        // }
-        // System.out.println(a);
-
-        //int a=0;
-        //for (int i = 0; i < array.length ; i++) {
-        //  a=a+array[i]/ array.length;
-        //  }
-        //System.out.println(a);
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("min=" + min);
 
 
-        //  for (int i = 0; i < array.length; i++) {
-        //    if (array[i] % 2 == 0)
-//
-        //              System.out.println(array[i]);
-        //            }
-        // for (int i = 0; i < array.length; i++) {
-        //   if (array[i]%2!=0) {
-        //       System.out.println(array[i]);
-        //  }
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println("max=" + max);
 
-        //  }
-        //int k = 0;
-        // for (int i = 0; i < array.length; i++) {
-        //   if (array[i] % 2 == 0) {
-        //      k++;
 
-        // }
+        int a = 0;
+        for (int i = 0; i < array.length; i++) {
+            a = a + array[i];
+        }
+        System.out.println(a);
 
-        // }System.out.print(k);
 
-        //int k = 0;
-        // for (int i = 0; i < array.length; i++) {
-        //   if (array[i] % 2 != 0) {
-        //       k++;
-        //   }
-        // }
-        //  System.out.println(k);
+        int b = 0;
+        for (int i = 0; i < array.length; i++) {
+            b = b + array[i] / array.length;
+        }
+        System.out.println(b);
+
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.println(array[i]);
+            }
+        }
+
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                System.out.println("kent tver" + array[i]);
+            }
+        }
+        int k = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                k++;
+            }
+        }
+        System.out.print(k);
+
+        int c = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                k++;
+            }
+        }
+        System.out.println(k);
     }
 }
--- src/classwork/Porcnakan.java
+++ src/classwork/Porcnakan.java
@@ -2,65 +2,32 @@ package classwork;
 
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
+import javax.swing.plaf.basic.BasicRadioButtonMenuItemUI;
+
 public class Porcnakan {
     public static void main(String[] args) {
-        int[] array = {7, 5, 4, 6, 8, -45, 879, 997, 32, 4, -56};
-        //    int a=array[0];
-        //    for (int i = 0; i < array.length; i++) {
-        //         if (array[i]< a){
-        //          a=array[i];
-        //        }
-        //  }
-        // System.out.println(a);
-        //  int a=array[0];
-        //for (int i = 0; i < array.length; i++) {
-        //   if (array[i]>a){
-        //       a=array[i];
-        //    }
-
-        // }
-        //System.out.println(a);
-        //int a =0;
-        // for (int i = 0; i < array.length; i++) {
-        //    a=a+array[i];
-        // }
-        // System.out.println(a);
-
-        //int a=0;
-        //for (int i = 0; i < array.length ; i++) {
-        //  a=a+array[i]/ array.length;
-        //  }
-        //System.out.println(a);
-
-
-        //  for (int i = 0; i < array.length; i++) {
-        //    if (array[i] % 2 == 0)
+        char[] chars = {' ', 'j', 'a', 'v', 'a', 'l', 'o', 'v', 'l', 'y'};
+//        for (int i = 0; i < chars.length; i++) {
 //
-        //              System.out.println(array[i]);
-        //            }
-        // for (int i = 0; i < array.length; i++) {
-        //   if (array[i]%2!=0) {
-        //       System.out.println(array[i]);
-        //  }
-
-        //  }
-        //int k = 0;
-        // for (int i = 0; i < array.length; i++) {
-        //   if (array[i] % 2 == 0) {
-        //      k++;
+//        }System.out.println(chars[chars.length/2] +" "+chars[chars.length/2+1]);
+//        for (int i = 0; i < chars.length; i++) {
+//            if (chars[i]!=' ')
+//                System.out.print(chars[i]);
 
         // }
+       for (int i = 0; i < chars.length; i++) {
+            if (chars.length - 1 == 'y' && chars.length - 2 == 'l') {
+                System.out.println(true);
+           }else System.out.println(false);
 
-        // }System.out.print(k);
+       }
 
-        //int k = 0;
-       // for (int i = 0; i < array.length; i++) {
-         //   if (array[i] % 2 != 0) {
-         //       k++;
-         //   }
-       // }
-      //  System.out.println(k);
+
     }
+
 }
+
+
+
 
 
