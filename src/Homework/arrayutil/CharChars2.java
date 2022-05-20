bff01e270e56004f7ac6daea746ed71ce50cd8a3
lenovo <hambaryan.93@bk.ru>
2022-05-20 23:38:09 +0400

    lesson 8,ArrayUtil

--- src/Homework/arrayutil/CharChars2.java
+++
@@ -1,8 +0,0 @@
-package Homework.arrayutil;
-
-public class CharChars2 {
-    public static void main(String[] args) {
-        char[] chars = {' ', 'j', 'a', 'v', 'a', 'l', 'o', 'v', 'l', 'y'};
-        System.out.println(chars[chars.length/2] +" "+chars[chars.length/2+1]);
-        }
-    }
