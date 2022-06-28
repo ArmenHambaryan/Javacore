d2289e21657c445a9da85823f6869d5212079bb6
lenovo <hambaryan.93@bk.ru>
2022-06-29 00:17:37 +0400

    homework.book

--- src/Homework/books/BooksDemo.java
+++ src/Homework/books/BooksDemo.java
@@ -35,7 +35,8 @@ public class BooksDemo implements commands {
                     printBooksByPriceRange();
                     break;
 
-
+                default:
+                    System.out.println("invalid command");
             }
 
         }
